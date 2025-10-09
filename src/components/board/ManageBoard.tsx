769e6abfe94632f1b2246294cd83b554cdc8b72f
Kushagra Sharma <sharmakartik3888@gmail.com>
2025-10-09 12:01:55 +0530

    folder naming changed and types are moved

--- src/components/board/ManageBoard.tsx
+++ src/components/board/ManageBoard.tsx
@@ -16,7 +16,7 @@ import { selectTypes } from "../../utils/constants/board";
 import { DEFAULT_COLORS } from "../../utils/constants/colors";
 import { toast } from "react-toastify";
 
-const CreateBoard: React.FC<BoardModalProp> = ({
+const ManageBoard: React.FC<BoardModalProp> = ({
   open,
   onClose,
   onCreate,
@@ -129,4 +129,4 @@ const CreateBoard: React.FC<BoardModalProp> = ({
   );
 };
 
-export default CreateBoard;
+export default ManageBoard;
