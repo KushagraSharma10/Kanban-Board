f8a3a6e08719ee8e22c1cdf4190febb7c55e8a02
Kushagra Sharma <sharmakartik3888@gmail.com>
2025-10-11 23:34:06 +0530

    pencil icon added for opening the edit model and instead of opening the card model from clicking on card , no card model opens after clicking on pencil icon

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -93,6 +93,14 @@ const Card: React.FC<CardProps> = ({ card, onUpdate, onDelete }) => {
           ref={menuRef}
           onClick={(e) => e.stopPropagation()}
         >
+           <button
+            aria-label="Edit card"
+            title="Edit"
+            onClick={handleOpenEditModal}
+            className="opacity-90 hover:opacity-100 hover:scale-[1.03] transition cursor-pointer"
+          >
+            <FiEdit2 />
+          </button>
           <button
             aria-label="Edit card"
             title="Edit"
